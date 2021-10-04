ce79a6f8ad3e39bbc7d0f65b3a5806f1faef339b
Sumeyye Nur <nurcaglayan@gmail.com>
2021-10-04 17:39:31 +0300

    forth

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import UserDetails from "./pages/UserDetails/UserDetails";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function Routes() {
   return (
